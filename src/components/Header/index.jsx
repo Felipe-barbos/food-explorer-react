ad385142cc2b6f137e22236dc5e8e6c0089d5730
Felipe-barbos <felipe.csultimate@gmail.com>
2023-12-13 15:03:10 -0300

    feat:atualizando header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,21 @@
-import { Container } from "./styles";
-
+import { Container, Menu } from "./styles";
+import { List, Receipt } from "@phosphor-icons/react";
 import backgroundImg from "../../assets/Icon.svg";
 
 
 export function Header() {
   return (
     <Container >
+
+
+      <Menu>
+        <List />
+      </Menu>
       <h1> <img src={backgroundImg} alt="" /> Food Explorer</h1>
+
+      <Menu>
+        <Receipt />
+      </Menu>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,21 +1,22 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 
 export const Container = styled.header`
 
-width: 100%;
-height: 5rem;
-
 display: flex;
-align-items: center;
-justify-content: center;
+align-items: flex-start;
+justify-content: space-between;
+padding: 1rem;
 
 
 background: ${({ theme }) => theme.COLORS.DARK_700};
 
 
-h1{
-  font-size: 2rem;
+
+
+> h1{
+  font-size: 1.5rem;
   display: flex;
   align-items: center;
   gap: 1rem;
@@ -29,5 +30,30 @@ h1{
 }
 
 
+
+`;
+
+
+export const Menu = styled.button`
+  background: none;
+  border:none;
+
+
+  >svg{
+    font-size: 2.5rem;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    
+  }
+
+
+  @media(max-width: ${DEVICE_BREAKPOINTS.MD}){
+    display: block;
+  }
+
+  @media(max-width: ${DEVICE_BREAKPOINTS.XS}){
+    > svg{
+      font-size: 2rem;
+    }
+  }
 
 `;
